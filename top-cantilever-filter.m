f93bc479bed27fe858e62aaf3b11d67c3d30200a
I 4M SuPr7m3 S1mpL0rd Voldemort <185697216+waifuneverdie@users.noreply.github.com>
2024-10-21 17:46:06 +0700

    Update top-cantilever-filter.m

--- top-cantilever-filter.m
+++ top-cantilever-filter.m
@@ -16,9 +16,9 @@ edofMat = repmat(edofVec,1,8)+repmat([0 1 2*nely+[2 3 0 1] -2 -1],nelx*nely,1);
 iK = reshape(kron(edofMat,ones(8,1))',64*nelx*nely,1);
 jK = reshape(kron(edofMat,ones(1,8))',64*nelx*nely,1);
 % DEFINE LOADS AND SUPPORTS (HALF MBB-BEAM)
-F = sparse(2,1,-1,2*(nely+1)*(nelx+1),1);
+F(2*(nelx+1)*(nely+1),1) = -1;
 U = zeros(2*(nely+1)*(nelx+1),1);
-fixeddofs = union([1:2:2*(nely+1)],[2*(nelx+1)*(nely+1)]);
+fixeddofs = [1:2*(nely+1)];
 alldofs = [1:2*(nely+1)*(nelx+1)];
 freedofs = setdiff(alldofs,fixeddofs);
 %% PREPARE FILTER
